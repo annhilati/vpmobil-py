fb802d9a65c2132022aa6433974cd091037f9922
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-04 19:52:46 +0200

    Update app.py

--- test-programm/app.py
+++ test-programm/app.py
@@ -4,6 +4,7 @@ from vpmobil import Stundenplan
 stundenplan = Stundenplan(10126582, "schueler", "s361o97")
 
 #tag = stundenplan.fetch(date=20240619)
-tag = stundenplan.fetch(date=20240215)
+tag = stundenplan.fetch(date=20240619)
 
 print(tag.getxml())
+print(tag.zeitstempel())
