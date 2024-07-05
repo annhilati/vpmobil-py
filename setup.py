dc74b09601c1400b8c8a5e0beb3fd1fe10e1b095
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-05 11:38:56 +0200

    Sync

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ setup(
     name="vpmobil",
     version="0.1",
     packages=find_packages(),
-    install_requires=["requests"],  # Hier können Abhängigkeiten aufgelistet werden
+    install_requires=["requests"],
     author="Annhilati & Joshi",
-    #author_email="Ihre Email",
     description="A python package for interacting with a stundenplan24.de substitution plan",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    #url="https://example.com/mypackage",  # URL zu Ihrem Projekt (optional)
+    url="https://github.com/annhilati/vpmobil-py"
 )
--- vpmobil/vertretungsplan.py
+++ vpmobil/vertretungsplan.py
@@ -1,8 +1,8 @@
-from .vpDay import VpDay
-
-from datetime import datetime
+from datetime import datetime, date
 #Modules shall be imported as a 3-letter code
+import xml.etree.ElementTree as XML 
 import requests as REQ 
+
 
 class Vertretungsplan():
     """
@@ -30,3 +30,68 @@ class Vertretungsplan():
             raise ValueError(f"Die Daten für das Datum {date} konnten nicht abgerufen werden. Statuscode: {response.status_code}")
 
         return VpDay(xmldata=response.content)
+    
+
+
+class VpDay():
+    """
+    Enthält alle Daten für einen bestimmten Tag 
+    """
+
+    def __init__(self, xmldata: XML.ElementTree | bytes | str):
+        self.datatree: XML.ElementTree = xmldata if isinstance(xmldata, XML.ElementTree) else XML.ElementTree(XML.fromstring(xmldata))
+
+    def getxml(self, format: str = "ElementTree") -> (XML.ElementTree | str):
+        """
+        Gibt alle Daten für den Tag in einem bestimmten Format zurück
+
+        - format: Das Format, in dem die Daten ausgegeben werden sollen. Eines von "str" oder "ElementTree".
+        """
+        
+        match format:
+            case "str":
+                return XML.tostring(self.datatree.getroot(), encoding="utf-8", method="xml").decode('utf-8')
+            case "ElementTree":
+                return self.datatree
+            case _:
+                raise ValueError(f"Nicht unterstütztes Format: {format}")
+
+    def zeitstempel(self) -> datetime:
+        """
+        Gibt den Zeitpunkt zurück, zu dem der Vertretungsplan veröffentlicht wurde
+        """
+        zeitstempel = self.datatree.find('Kopf/zeitstempel').text
+        return datetime.strptime(zeitstempel, "%d.%m.%Y, %H:%M")
+    
+    def zusatzInfo(self) -> str:
+        """
+        Gibt die Zusatzinformationen des Tages zurück
+        """
+        return self.datatree.find("ZusatzInfo/ZiZeile").text
+
+    def freieTage(self) -> list[date]:
+        vpMobil = self.datatree.getroot()
+        freieTageElement = vpMobil.find("FreieTage")
+        if freieTageElement is None:
+            raise ValueError("Element 'FreieTage' nicht in den XML-Daten gefunden")
+        
+        freieTage: list[date] = []
+        for ft in freieTageElement.findall("ft"):
+            if ft.text is not None:
+                freieTage.append(datetime.strptime(ft.text, "%y%m%d").date())
+        return freieTage
+            
+    def klasse(self, class_short: str) -> XML.Element:
+        """
+        Gibt das XML-Element der angegebenen Klasse zurück.
+        Ein Fehler wird ausgegeben, wenn die angegebene Klasse nicht gefunden werden kann. 
+
+        - class_short: Kürzel der zu suchenden Klasse (z.B. "8b")
+        """
+        vpmobil = self.datatree.getroot()
+        for kl in vpmobil.findall('.//Kl'):
+            kurz = kl.find('Kurz')
+            if kurz is not None and kurz.text == class_short:
+                return kl
+        raise ValueError(f"Keine Klasse {class_short} gefunden")
+        
