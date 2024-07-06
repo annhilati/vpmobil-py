a605e5bd0a01b655ae3799455086a202ad9799e1
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-06 18:25:22 +0200

    Sync

--- vpmobil/VpDay.py
+++ vpmobil/VpDay.py
@@ -1,42 +1,9 @@
 from datetime import datetime, date
 #Modules shall be imported as a 3-letter code
 import xml.etree.ElementTree as XML 
-import requests as WEB
 
-from .Exceptions import FetchingError, XMLError
+from .Exceptions import XMLError
 
-# ╭──────────────────────────────────────────────────────────────────────────────────────────╮
-# │                                    Vertretungsplan                                       │ 
-# ╰──────────────────────────────────────────────────────────────────────────────────────────╯
-
-class Vertretungsplan():
-    """
-    Enthält die notwendigen Daten um auf einen stundenplan24.de-Vertretungsplan zuzugreifen
-    """
-
-    def __init__(self, schulnummer: int, benutzername: str, passwort: str):
-        self.schulnummer = schulnummer
-        self.benutzername = benutzername
-        self.passwort = passwort
-        self.webpath = f"http://{benutzername}:{passwort}@stundenplan24.de/{schulnummer}/mobil/mobdaten/"
-
-    def fetch(self, date: int | date = datetime.today().date()):
-        """
-        Ruft alle Daten für einen bestimmten Tag ab und schreibt sie in ein VpDay-Objekt
-        Ein Error wird erhoben, wenn für den angegebenen Tag keine Daten verfügbar sind.
-
-        - date: Bestimmter Tag; Integer im yyyymmdd-Format oder date-Objekt. Leer lassen, um das heutige Datum abzurufen
-        """
-
-        date = date if isinstance(date, int) else date.strftime('%Y%m%d')
-        uri = f"{self.webpath}PlanKl{date}.xml"  
-        response = WEB.get(uri)
-
-        if response.status_code != 200:
-            raise FetchingError(f"Die Daten für das Datum {date} konnten nicht abgerufen werden. Statuscode: {response.status_code}")
-
-        return VpDay(xmldata=response.content)
-    
 # ╭──────────────────────────────────────────────────────────────────────────────────────────╮
 # │                                         VpDay                                            │ 
 # ╰──────────────────────────────────────────────────────────────────────────────────────────╯
@@ -109,3 +76,76 @@ class VpDay():
             if ft.text is not None:
                 freieTageList.append(datetime.strptime(ft.text, "%y%m%d").date())
         return freieTageList
+
+class Stunde():
+    """
+    Enthält Informationen über eine bestimmte Stunde
+    """
+
+    def __init__(self, elem: XML.Element | bytes | str):
+        self.std = elem if isinstance(elem, XML.Element) else XML.Element(XML.fromstring(elem))
+        self.nr : int = int(self.std.find("St").text)
+        """
+        Nummer der Stunde
+        """
+
+        self.beginn : str = str(self.std.find("Beginn").text)
+        """
+        Beginn der Stunde als str()
+        """
+
+        self.ende : str = str(self.std.find("Ende").text)
+        """
+        Ende der Stunde als str()
+        """
+
+        if "FaAe" in self.std.find("Fa").attrib or "RaAe" in self.std.find("Ra").attrib or "LeAe" in self.std.find("Le").attrib:
+            anders = True
+        else:
+            anders = False
+        self.anders : bool = anders
+        """
+        Gibt an, ob irgendeine Eigenschaft dieser Stunde geändert ist
+        """
+
+        if self.std.find("Fa").text == "---":
+            ausfall = True
+        else:
+            ausfall = False
+        self.entfaellt : bool = ausfall
+        """
+        Gibt an, ob die Stunde entfällt. 
+        Wenn ja, werden 'lehrer', 'fach' und 'raum' leere Strings zurückgeben
+        """
+
+        self.fach : str = self.std.find("Fa").text if self.entfaellt == False else ""
+        """
+        Gibt das Fach, welches in dieser Stunde stattfindet zurück.
+        Gibt einen leeren String zurück, wenn die Stunde entfällt
+        """
+
+        self.lehrer : str = self.std.find("Le").text if self.entfaellt == False else ""
+        """
+        Gibt den Lehrer, welcher diese Stunde hält zurück.
+        Gibt einen leeren String zurück, wenn die Stunde entfällt
+        """
+
+        self.raum : str = self.std.find("Ra").text if self.entfaellt == False else ""
+        """
+        Gibt den Raum, in dem diese Stunde stattfindet zurück.
+        Gibt einen leeren String zurück, wenn die Stunde entfällt
+        """
+
+        self.kursnummer : str = self.std.find("Nr").text
+        """
+        Gibt die Nummer des Kurses zurück.
+        Nützlich für das Kurs() Objekt
+        """
+
+        self.info : str = self.std.find("If").text
+        """
+        Gibt eine optionale vom Planer verfasste Information zu dieser Stunde.
+        Ist nur in besonderen Situationen und bei entfallen der Stunde vorhanden
+        """
+
+...
--- vpmobil/__init__.py
+++ vpmobil/__init__.py
@@ -2,8 +2,8 @@
 A python module for interacting with a stundenplan24.de substitution plan
 """
 
-from .Vertretungsplan import Vertretungsplan, VpDay
+from .Vertretungsplan import Vertretungsplan, VpDay, Stunde
 
-__all__ = ['Vertretungsplan', "VpDay"]
+__all__ = ['Vertretungsplan', "VpDay", "Stunde"]
     # Enthält alle Symbole, die bei "from vpmobil import" verfügbar sind
     # Enthält alle Symbole, die bei "from vpmobil import *" importiert werden
