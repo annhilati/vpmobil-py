12f85232bec2019db94d3bfd0ace27738cdd5b20
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-07 21:43:43 +0200

    Sync

--- tests/tooltips.py
+++ tests/tooltips.py
@@ -1,14 +1,17 @@
 # In dieser Datei können Tooltips, Docstrings und Typehints getestet werden
 
-from vpmobil import Vertretungsplan, VpDay
+from vpmobil import Vertretungsplan, VpDay, Stunde
 
 vp = Vertretungsplan()
+day = VpDay()
+stunde = Stunde()
+
+
 vp.fetch()
 vp.schulnummer 
 
-day = VpDay()
 day.getxml()
 day.klasse()
 day.freieTage()
-day.zusatzInfo()
-day.zeitstempel()
+day.zusatzInfo
+day.zeitstempel
