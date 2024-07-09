7ede4313bfb56ad97d55e659e98d3c10984b163d
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-09 21:27:26 +0200

    Update app.py

--- tests/app.py
+++ tests/app.py
@@ -5,12 +5,14 @@ from datetime import date
 
 vertretungsplan = Vertretungsplan(10126582, "schueler", "s361o97")
 
-for d in range(20240612, 20240620):
-    try:
-        day = vertretungsplan.fetch(d)
-        try:
-            print(day.lehrerKrank())
-        except:
-            print(f"Error der Lehrer an {d}")
-    except:
-        print(f"Kein Plan für {d}")
+# for d in range(20240612, 20240620):
+#     try:
+#         day = vertretungsplan.fetch(d)
+#         try:
+#             print(day.lehrerKrank())
+#         except:
+#             print(f"Error der Lehrer an {d}")
+#     except:
+#         print(f"Kein Plan für {d}")
+
+print(vertretungsplan.fetch(20240614).lehrerKrank())
