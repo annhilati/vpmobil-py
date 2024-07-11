8f6daf6a74c83a6914d9ea2c5e31d0d6253a159e
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-11 13:53:28 +0200

    Sync

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as readme:
+    description = readme.read() 
+
 setup(
     name="vpmobil",
     version="0.1",
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=[
+        "requests"
+        ],
     author="Annhilati & Joshi",
     description="A python package for interacting with a stundenplan24.de substitution plan",
-    long_description=open('README.md').read(),
+    long_description=description,
     long_description_content_type="text/markdown",
     url="https://github.com/annhilati/vpmobil-py"
 )
