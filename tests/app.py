761e2e53e1c7a05db38092506fdff7aa2271e1e3
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-18 13:02:53 +0200

    Sync

--- tests/app.py
+++ tests/app.py
@@ -2,7 +2,7 @@ from vpmobil import Vertretungsplan, VpMobil, VpDay, Klasse
 import xml.etree.ElementTree as XML
 import os
 
-vertretungsplan = Vertretungsplan(10126582, "schueler", "s361o97", "https://stundenplan24.de/10126582/mobil/mobdaten", "PlanKl%Y%m%d.xml")
+vertretungsplan = Vertretungsplan(10126582, "schueler", "s361o97")
 
 day = vertretungsplan.fetch(20240619)
 klasse = day.klasse("9a")
