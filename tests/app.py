bad6afa221b2d8a7748a628192f3062c90648b36
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-30 21:06:44 +0200

    Update app.py

--- tests/app.py
+++ tests/app.py
@@ -6,5 +6,11 @@ import requests
 from vpmobil import Vertretungsplan
 
 vp = Vertretungsplan(10126582, "schueler", "s361o97")
+vp1 = Vertretungsplan(10161728, "schueler", "23LgS24")
 
-file = vp.fetch(datum=20240619)
+for date in range(20240701, 20240730):
+    try:
+        tag = vp1.fetch(date)
+        tag.saveasfile(f"./scrap/{date}.xml")
+    except VpMobil.FetchingError:
+        print(f"{date} konnte keinen Plan hervorbringen")
