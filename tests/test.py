ea980d5d07a7de05b5cf6adfc2b77924a6108374
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-07-31 11:06:02 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,8 @@
 import requests
 
-req = requests.get("http://schueler:s361o97@stundenplan24.de/10126582/mobil/mobdaten/PlanKl20240619.xml")
+req = requests.get(url="http://schueler:s361o97@stundenplan24.de/10126582/mobil/mobdaten/PlanKl20240619.xml")
 
 print(req.content)
+
+if 4 == 2 and 4 + 5:
+    pass
